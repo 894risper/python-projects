8a7695513140c1c76410e3232ec0bb86a19fa949
894risper <zilper894@gmail.com>
2024-09-24 12:08:53 +0300

    python basics

---
+++ temp.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr= np.array([1,2,3,4,5,6])
+print(arr)
+print(type(arr))
+
+grades=[60,50,70,100]
+total=sum(grades)
+print( "the total grase is:",total)
+
+length= len(grades)
+print("the length of the list is:",length)
+print(type(grades))
